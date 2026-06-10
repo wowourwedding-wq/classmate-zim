fa4ceaf70c4cba772eba04f1924607f5045aa3d0
Lilian | AI Solutions & Business Systems <wowourwedding@gmail.com>
2026-06-10 22:55:21 +0200

    feat: Subject Register + Meeting Minutes template + CSV import for logs (Zim v46/v36/v20)

--- teacher-planner-zim/sw.js
+++ teacher-planner-zim/sw.js
@@ -9,7 +9,7 @@
                                       don't have to re-download after updates.
 */
 
-const SHELL_CACHE = 'classmate-zim-v45';
+const SHELL_CACHE = 'classmate-zim-v46';
 const PAPERS_CACHE = 'classmate-zim-papers-v1';
 const SHELL = ['./', './index.html'];
 
