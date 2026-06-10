0c4ee0c128ec6f44cbd81e97f4d16e763afe459d
Lilian | AI Solutions & Business Systems <wowourwedding@gmail.com>
2026-06-10 22:30:28 +0200

    feat: Question Bank CSV import (Zim v44/v34/v18)

--- teacher-planner-zim/sw.js
+++ teacher-planner-zim/sw.js
@@ -9,7 +9,7 @@
                                       don't have to re-download after updates.
 */
 
-const SHELL_CACHE = 'classmate-zim-v43';
+const SHELL_CACHE = 'classmate-zim-v44';
 const PAPERS_CACHE = 'classmate-zim-papers-v1';
 const SHELL = ['./', './index.html'];
 
