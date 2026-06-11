6aa25ba72126960611b736786af8bc9e66686eaa
Lilian | AI Solutions & Business Systems <wowourwedding@gmail.com>
2026-06-11 15:50:57 +0200

    feat: Letters consolidated tabs + Save/WhatsApp + Worksheet view + Key Parents+Leadership CSV + print fixes (Zim v55/v45/v29)

--- teacher-planner-zim/sw.js
+++ teacher-planner-zim/sw.js
@@ -9,7 +9,7 @@
                                       don't have to re-download after updates.
 */
 
-const SHELL_CACHE = 'classmate-zim-v54';
+const SHELL_CACHE = 'classmate-zim-v55';
 const PAPERS_CACHE = 'classmate-zim-papers-v1';
 const SHELL = ['./', './index.html'];
 
